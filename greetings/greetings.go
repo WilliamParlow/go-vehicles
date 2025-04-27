08f71433e753d31da4469bc2145e1e3d71f9cfff
William Parlow Nichelatti <williamn@weg.net>
2025-04-26 22:31:09 -0300

    refactor greetings and hello modules to handle errors and randomize greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,26 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hello, %s!", name)
-	return message
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hello, %v!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("William")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello(randomName())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
+}
+
+func randomName() string {
+	names := []string{
+		"William",
+		"James",
+		"",
+	}
+	return names[rand.Intn(len(names))]
 }
